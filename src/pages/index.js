4d9f8effcf0d814dbde6d8d2eaefb927e7473263
Rodrigo Muniz <rodrigo@muniz>
2019-08-06 00:11:57 -0300

    Coisas para arrumar

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,19 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
+import { graphql } from "gatsby"
+import PostLink from "../components/post-link"
+
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+  
+  return <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
@@ -14,8 +25,25 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <div>{Posts}</div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
-
+}
 export default IndexPage
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
---
+++ src/templates/blog-list.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/Layout/'
+import SEO from '../components/Seo'
+import Post from '../components/Post'
+import Pagination from '../components/Pagination'
+
+import * as S from '../components/ListWrapper'
+
+const BlogList = props => {
+  const postList = props.data.allMarkdownRemark.edges
+
+  const { currentPage, numPages } = props.pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage = currentPage - 1 === 1 ? '/' : `/page/${currentPage - 1}`
+  const nextPage = `/page/${currentPage + 1}`
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <S.ListWrapper>
+        {postList.map(({ node }, i) => (
+          <Post
+            key={i}
+            slug={node.fields.slug}
+            title={node.frontmatter.title}
+            timeToRead={node.timeToRead}
+            date={node.frontmatter.date}
+            description={node.frontmatter.description}
+            main_class={node.frontmatter.main_class}
+          />
+        ))}
+      </S.ListWrapper>
+
+      <Pagination
+        currentPage={currentPage}
+        numPages={numPages}
+        isFirst={isFirst}
+        isLast={isLast}
+        prevPage={prevPage}
+        nextPage={nextPage}
+      />
+    </Layout>
+  )
+}
+
+export default BlogList
+
+export const PostListQuery = graphql`
+  query PostListQuery($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      sort: { fields: frontmatter___date, order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+            description
+            main_class
+            title
+          }
+          timeToRead
+        }
+      }
+    }
+  }
+`
