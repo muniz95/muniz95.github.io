284daf0c6181e1b199f1506f0b4e7972a4c51b26
Rodrigo Muniz <rtmuniz@positivo.com.br>
2019-08-09 13:05:32 -0300

    Tema claro e escuro alterados

--- src/styles/global.js
+++ src/styles/global.js
@@ -65,11 +65,11 @@ const GlobalStyles = createGlobalStyle`
 
   body.dark {
     --borders: #38444d;
-    --texts: #8899a6;
+    --texts: #ffffff;
     --postColor: #fff;
-    --highlight: #1fa1f2;
-    --mediumBackground: #192734;
-    --background: #16202c;
+    --highlight: #f2e21f;
+    --mediumBackground: #18191b;
+    --background: #090e09;
     --white: #fff;
     --black: #222;
   }
@@ -78,7 +78,7 @@ const GlobalStyles = createGlobalStyle`
     --borders: #dedede;
     --postColor: #111;
     --texts: #555555;
-    --highlight: #1fa1f2;
+    --highlight: #0066e4;
     --mediumBackground: #f0f0f3;
     --background: #fff;
     --white: #fff;
